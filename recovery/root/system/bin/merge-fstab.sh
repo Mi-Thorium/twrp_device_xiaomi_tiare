056a1b001b4b61f71bc532e8156f223ac5e34896
me-cafebabe <me_cafebabe@hotmail.com>
2022-08-05 18:16:18 +0800

    tiare: Support Retrofit Dynamic Partitions

--- recovery/root/system/bin/merge-fstab.sh
+++ recovery/root/system/bin/merge-fstab.sh
@@ -3,3 +3,13 @@ echo >> /system/etc/recovery.fstab
 cat /system/etc/recovery.fstab.device >> /system/etc/recovery.fstab
 echo >> /system/etc/twrp.flags
 cat /system/etc/twrp.flags.device >> /system/etc/twrp.flags
+
+# Dynamic Partitions
+if dd if=/dev/block/by-name/system bs=256k count=1|strings|grep tiare_dynpart > /dev/null; then
+    sed -i 's|/dev/block/bootdevice/by-name/cache|/dev/block/bootdevice/by-name/logdump|g' /system/etc/twrp.flags
+    echo >> /system/etc/recovery.fstab
+    cat /system/etc/recovery.fstab.dynpart >> /system/etc/recovery.fstab
+else
+    echo >> /system/etc/twrp.flags
+    cat /system/etc/twrp.flags.nondynpart >> /system/etc/twrp.flags
+fi
